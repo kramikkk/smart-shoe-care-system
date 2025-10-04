24675b2e1c8260955499fe9a18e4a85445102879
Kramik <markexconde31@gmail.com>
2025-10-04 22:30:38 +0800

    feat: add LoginForm component and edit LoginPage.

--- frontend/smart-shoe-care-system/app/page.tsx
+++ frontend/smart-shoe-care-system/app/page.tsx
@@ -1,7 +1,29 @@
-const page = () => {
+import Image from "next/image"
+import { LoginForm } from "@/components/LoginForm"
+
+const LoginPage = () => {
   return (
-    <div>Homepage</div>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <div className="flex flex-col items-center gap-1">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <Image
+              src="/globe.svg"
+              alt="Smart Shoe Care Logo"
+              width={30}
+              height={30}
+              className="rounded-md"
+            />
+            <h1 className="text-xl font-bold">Smart Shoe Care Machine</h1>
+          </a>
+          <p className="text-sm text-muted-foreground">
+            An IoT automated cleaning, drying, and sterilizing machine
+          </p>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
   )
 }
 
-export default page
+export default LoginPage
